474d40bd56a81b64c3ed990c8f0762d92234b7d6
vivek-sundli <viveksundli@github.com>
2025-10-23 00:27:18 +0530

    initial commit springPhysio

---
+++ src/main/java/com/healthclub/Physioplus/PhysioplusApplication.java
@@ -0,0 +1,13 @@
+package com.healthclub.Physioplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysioplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysioplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthclub/Physioplus/PhysioplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthclub.Physioplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysioplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
